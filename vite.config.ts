2b7baee83908c34c4ea3a3fa8dc6765474793f95
Marko Nikolic <markoirmes@gmail.com>
2026-07-04 03:37:04 +0200

    Add type annotation to resolveId function

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
+    resolveId(id: string) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '');
         return path.resolve(__dirname, 'src/assets', filename);
@@ -41,6 +41,5 @@ export default defineConfig({
     emptyOutDir: true,
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 });
